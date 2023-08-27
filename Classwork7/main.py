fed6b560775aa3da213d0255cc23155693e45543
AndreiShimLeon <art.shim@yandex.ru>
2023-08-27 19:36:56 +0300

    homework done

--- Classwork7/main.py
+++ Classwork7/main.py
@@ -6,6 +6,55 @@
 # для поиска определенной записи (например, имя или фамилию)
 # 4. Использование функций. Программа не должны быть линейной.
 
+
+# Homework: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию,
+# и Вы должны реализовать функционал для изменения и удаления данных
+
+def correct_data(phonebook):
+    flag = True
+    while flag:
+        search_key = input('Введите имя/фамилию для поиска. Для выхода введите "выход": ')
+        if search_key.lower() == 'выход':
+            flag = False
+        else:
+            results = search_contacts(phonebook, search_key)
+            if len(results) > 1:
+                print('Было найдено более 1 записи, повторите поиск')
+            else:
+                print(f'{results[0][1]["last_name"]},{results[0][1]["first_name"]},{results[0][1]["middle_name"]},{results[0][1]["phone_number"]}')
+                confirmation = input(f'Найденная запись требует корректировки? Y/N: ').lower()
+                if confirmation == 'y':
+                    new_l_name = input('Введите новую фамилию: ')
+                    new_f_name = input('Введите новое имя: ')
+                    new_m_name = input('Введите новое отчество: ')
+                    new_number = input('Введите новый номер: ')
+                    new_contact = {
+                        'last_name': new_l_name,
+                        'first_name': new_f_name,
+                        'middle_name': new_m_name,
+                        'phone_number': new_number
+                    }
+                    phonebook[results[0][0]] = new_contact
+                    print('Запись скорректирована')
+
+def delete_data(phonebook):
+    flag = True
+    while flag:
+        search_key = input('Введите имя/фамилию для поиска. Для выхода введите "выход": ')
+        if search_key.lower() == 'выход':
+            flag = False
+        else:
+            results = search_contacts(phonebook, search_key)
+            if len(results) > 1:
+                print('Было найдено более 1 записи, повторите поиск')
+            else:
+                print(
+                    f'{results[0][1]["last_name"]},{results[0][1]["first_name"]},{results[0][1]["middle_name"]},{results[0][1]["phone_number"]}')
+                confirmation = input(f'Вы хотите удалить запись? Y/N: ').lower()
+                if confirmation == 'y':
+                    phonebook.pop(results[0][0])
+                    print('Запись удалена')
 def load_file(filename):
     phonebook=[]
     try:
@@ -16,7 +65,7 @@ def load_file(filename):
                     'last_name': last_name,
                     'first_name': first_name,
                     'middle_name': middle_name,
-                    'phone_number': phone_number
+                    'phone_number': phone_number.replace('\n', '')
                 })
             print('Данные загружены')
     except FileNotFoundError:
@@ -25,9 +74,9 @@ def load_file(filename):
     return phonebook
 def search_contacts(phonebook, search_key):
     results = []
-    for contact in phonebook:
+    for index, contact in enumerate(phonebook):
         if search_key.lower() in contact['last_name'].lower()  or search_key.lower() in contact['first_name'].lower():
-            results.append(contact)
+            results.append([index,contact])
     if len(results) == 0:
         print(f'Запись не найдена') 
     else:
@@ -67,7 +116,9 @@ def main ():
         print("3. Вывести все контакты")
         print("4. Поиск по имени/фамилии")
         print("5. Загрузить из файла")
-        print("6. Выйти")
+        print("6. Скорректировать данные")
+        print("7. Удалить данные")
+        print("8. Выйти")
         print("=" * 100)
         choice = input('Выберите действие: ')
         if choice == "1":
@@ -83,10 +134,15 @@ def main ():
         elif choice == "4":
             search_key = input('Введите имя/фамилию для поиска: ')
             results = search_contacts(phonebook, search_key)
-            views_contacts(results)
+            for contact in results:
+                print(f'{contact[1]["last_name"]},{contact[1]["first_name"]},{contact[1]["middle_name"]},{contact[1]["phone_number"]}')
         elif choice == '5':
             phonebook = load_file(filename)
-        elif choice == "6":
+        elif choice == '6':
+            correct_data(phonebook)
+        elif choice == '7':
+            delete_data(phonebook)
+        elif choice == "8":
             flag = False
         else:
             print("Некорректный выбор. Повторите ввод.")
